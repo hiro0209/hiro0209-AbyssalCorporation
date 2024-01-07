d6a84c0fbe6ed138ecc93e39a958d85c9327b0e2
hiro0209 <budouraamen0209@gmail.com>
2024-01-07 18:37:39 +0900

    トップページか否かでヘッダーのスタイルを変更できるように

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -1,8 +1,18 @@
 <template>
-  <header>
+  <header :class="props.topPage ? 'top_page' : ''">
     
   </header>
 </template>
 
 <style type="scss">
 </style>
+
+<script setup>
+// Props
+const props = defineProps({
+  "top-page": {
+    type: Boolean,
+    default: false
+  }
+});
+</script>
